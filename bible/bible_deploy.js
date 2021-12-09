021b321da525843cda55b2470b2f3ccf59e40081
“JaredMathis” <wordslikejared@gmail.com>
2021-12-08 22:43:28 -0500

    2021-12-09T03:43:28.232Z

--- bible/bible_deploy.js
+++ bible/bible_deploy.js
@@ -5,7 +5,7 @@ import file_copy from '../core/file_copy.js';
 let directory_firebase_deploy = directory_firebase_deploy_get();
 git_acp();
 command_line_and_log(`rm -R ${directory_firebase_deploy}`)
-command_line_and_log(`node ../c/r file_js_deploy ui_bible_flash_root ${ directory_firebase_deploy }`);
+command_line_and_log(`node ../c/r file_js_deploy ui_bible_root ${ directory_firebase_deploy }`);
 file_copy('./package.json', `${directory_firebase_deploy}/package.json`);
 command_line_and_log(`cd ${directory_firebase_deploy} && npm i`)
 command_line_and_log(`firebase deploy`);
