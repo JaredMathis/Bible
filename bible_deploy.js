dc18203d4045c8b338efe981ac741d847bb6e8cb
“JaredMathis” <wordslikejared@gmail.com>
2021-12-21 22:55:21 -0500

    2021-12-22T03:55:21.867Z

--- bible_deploy.js
+++ bible_deploy.js
@@ -1,3 +1,2 @@
 import deploy_firebase from './core/deploy_firebase.js';
-import ui_bible_root from './ui_bible/ui_bible_root.js';
-deploy_firebase(ui_bible_root);
+deploy_firebase('ui_bible_root');
